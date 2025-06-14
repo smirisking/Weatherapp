0608f1f1a7f0dab81bb8c3d3d9d6b7f46979530c
Samir <Samirhaidari400@gmail.com>
2025-06-14 16:52:51 -0400

    finished it today

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
-st.title("Weather Forcast for the Next Days")
+st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
-                 help="Select the number of days you want to forcast")
-option = st.selectbox("Select date to view", ("Temperature", "Sky Condition"))
+                 help="Select the number of days you want to forecast")
+option = st.selectbox("Select data to view", ("Temperature", "Sky Condition"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
+
+if place:
+    try:
+        filtered_data = get_data(place, days)
+
+        if option == "Temperature":
+            temperatures = [((d["main"]["temp"] / 10) * 9 / 5 + 32) for d in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (F)"})
+            st.plotly_chart(figure)
+
+        elif option == "Sky Condition":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_path = [images[condition] for condition in sky_condition]
+            st.image(image_path, width=115)
+
+    except:
+        st.error("You entered a city that doesn't exist. Please check the spelling.")
+
+
